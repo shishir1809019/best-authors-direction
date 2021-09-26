3ab5e3ae26fab64810db2b35d1ee2cff917c7ba8
shishir1809019 <shishirdas726@gmail.com>
2021-09-26 21:18:19 +0600

    header edit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # the-superhero-direction-shishir1809019
 
 live website: https://the-superhero-direction1999.netlify.app/
+
+- In this website I have used **Javascript, React, Bootstrap & HTML, CSS**
+- this website is listed some popular author name and description.
+- By this website you can meet up with those famous authors.
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         <h1 style={{ fontWeight: "700" }}>Best Authors list</h1>
         <h3 className="">Choose your favorite author</h3>
         <p>Mesmerizing Books for Fans of All the Light We Cannot See</p>
+        <h5 className="m-0 p-0">Total Budget: 100 Million</h5>
       </div>
     </div>
   );
